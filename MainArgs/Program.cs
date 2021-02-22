eac9cbf962c45cc15d2a31cd437e35c171faaed4
dimanmup <dimitry.myagkov@gmail.com>
2021-02-22 21:05:10 +0300

    0.2.0.0 MainArgs

---
+++ MainArgs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MainArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
