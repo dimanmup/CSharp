e4f9c502ce79f6a6869bc98ff6d60f2467d13a2b
dimanmup <dimitry.myagkov@gmail.com>
2021-03-09 21:16:49 +0300

    0.5.0.0 + Types_IntsAssignments

---
+++ Types_IntsAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Types_IntsAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x0;
+            int x1 = 1;
+            int x2 = x1 + 1, x3 = x1 + x2;
+
+            // Синтаксическая ошибка: переменной x0 не присвоено значение.
+            //Console.WriteLine("x0 : " + x0);
+            
+            Console.WriteLine("x1 : " + x1);
+            Console.WriteLine("x2 : " + x2);
+            Console.WriteLine("x3 : " + x3);
+
+            /*
+            x1 : 1
+            x2 : 2
+            x3 : 3
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Types_IntsAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Types_IntsAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Types_IntsAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7495c07-1af6-4a39-9e97-6650c9a59446")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
