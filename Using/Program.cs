5d91aeb267280b95da7bac72948688e6048855ee
dimanmup <dimitry.myagkov@gmail.com>
2021-03-09 20:21:02 +0300

    0.3.0.0 + Using

---
+++ Using/Program.cs
@@ -0,0 +1,16 @@
+﻿using Sys = System; // (1)
+
+namespace Using
+{
+    using Cons = System.Console; // (2)
+
+    class Program
+    {
+        static void Main()
+        {
+            Sys.Console.WriteLine("Sys");
+            Cons.WriteLine("Cons");
+            System.Console.ReadKey();
+        }
+    }
+}
