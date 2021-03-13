7a9b2eab31b61f9fa3a95a034b6bb48b6a772bb7
dimanmup <dimitry.myagkov@gmail.com>
2021-03-13 12:46:50 +0300

    0.8.0.0 + Types_Ints_

---
+++ Types_Ints_/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Types_Ints_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(1_000_000);
+            Console.WriteLine(0x0000_00fF);
+            Console.WriteLine(0b0000_0010);
+
+            Console.ReadKey();
+        }
+    }
+}
