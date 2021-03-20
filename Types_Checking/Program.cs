7234222c51fe52d96c1a3a4c102a63848f1eb2bd
dimanmup <dimitry.myagkov@gmail.com>
2021-03-20 13:03:23 +0300

    0.9.0.0 + Types_Checking

---
+++ Types_Checking/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Types_Checking
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte err, min, max;
+
+            // Ошибки.
+            /*
+            err = (byte)(byte.MaxValue + 1);
+            checked
+            {
+                err = (byte)(byte.MaxValue + 1);
+            }
+            err = checked((byte)(byte.MinValue - 1));
+            */
+
+            unchecked
+            {
+                min = (byte)(byte.MaxValue + 1);
+            }
+            max = unchecked((byte)(byte.MinValue - 1));
+
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(min);
+            Console.WriteLine("---");
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(max);
+
+            /*
+            0
+            0
+            ---
+            255
+            255 
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
