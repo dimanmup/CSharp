f1473607025f911ef6f233a6b8cc1cf9c9fbca00
dimanmup <dimitry.myagkov@gmail.com>
2021-03-21 23:39:06 +0300

    0.12.0.0 + Unity_Vector3_Operators

---
+++ Unity_Vector3_Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity_Vector3_Operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            Vector3 v1 = new Vector3(2, 4, 6);
+            Vector3 v2 = new Vector3(1, 3, 5);
+
+            Console.WriteLine(v1 + v2);
+            // (2+1, 4+3, 6+5)
+            // (3, 7, 11) 
+
+            Console.WriteLine(v1 - v2);
+            // (2-1, 4-3, 6-5)
+            // (1, 1, 1) 
+
+            Console.WriteLine(v1 * 2);
+            // (2*2, 4*2, 6*2)
+            // (4, 8, 12)
+
+            Vector3 v1_ = new Vector3(2, 4, 6);
+            Console.WriteLine(v1 == v1_);
+            // True
+
+            Console.ReadKey();
+        }
+    }
+}
