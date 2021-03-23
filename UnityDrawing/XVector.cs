b0015a233dc16a55b59234fb3b9217d03e7371d0
dimanmup <dimitry.myagkov@gmail.com>
2021-03-23 17:06:20 +0300

    0.16.0.0 + Unity_Vector2_ViewOnGrid + UnityDrawing XVector.cs

---
+++ UnityDrawing/XVector.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using UnityEngine;
+using Color = System.Drawing.Color;
+using Graphics = System.Drawing.Graphics;
+
+namespace UnityDrawing
+{
+    public static class XVector
+    {
+        public static void Write(this Vector2 vector, Color color, int width, Graphics gr)
+        {
+            Pen pen = new Pen(color, width);
+            pen.CustomEndCap = new AdjustableArrowCap(5, 5);
+            gr.DrawLine(pen, 0, 0, vector.x, vector.y);
+        }
+    }
+}
---
+++ Unity_Vector2_ViewOnGrid/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using UnityDrawing;
+using UnityEngine;
+using Color = System.Drawing.Color;
+using Grid = UnityDrawing.Grid;
+
+namespace Unity_Vector2_ViewOnGrid
+{
+    public partial class Form1 : Form
+    {
+        Panel panel = new Panel();
+        Grid grid;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Controls.Add(panel);
+
+            grid = new Grid();
+            Vector2 v = new Vector2(300, 300);
+            v.Write(Color.Red, 2, grid.Graphics);
+            grid.Fill(panel);
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Vector2 v = new Vector2(400, 200);
+            v.Write(Color.Blue, 2, grid.Graphics);
+            grid.Fill(panel);
+
+            this.Controls.Remove(button1);
+        }
+    }
+}
