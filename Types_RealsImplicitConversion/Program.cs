73b9bdae37df4cf691695de8bdca878b87600581
Diman <Diman@DESKTOP-CPKMC9C>
2021-04-24 14:37:16 +0300

    0.17.0.0 + Types_RealsImplicitConversion + Types_RealsVar

---
+++ Types_RealsImplicitConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Types_RealsImplicitConversion
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1
+            float f = 1;
+            double d = 20;
+            decimal m = 300;
+
+            // Навести курсор мыши на var.
+            // 2
+            var f_result = f + 1; // float
+            var d_result = d + f + 1; // double
+            var m_result = m + 1; // decimal
+            //var x = m + f; Ошибка компиляции.
+        }
+    }
+}
---
+++ Types_RealsVar/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Types_RealsVar
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Навести курсор мыши на var.
+            var f = 1f;
+            var d1 = 1.0;
+            var d2 = 1d;
+            var m = 1m;
+        }
+    }
+}
