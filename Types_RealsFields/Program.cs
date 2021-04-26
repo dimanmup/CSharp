f98c32939970d0e3fe469dbd0ed2854de2625a81
Diman <Diman@DESKTOP-CPKMC9C>
2021-04-26 19:00:19 +0300

    0.18.0.0 + Types_RealsFields

---
+++ Types_RealsFields/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Types_RealsFields
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("float.Epsilon: " + float.Epsilon);
+            Console.WriteLine("float.MaxValue: " + float.MaxValue);
+            Console.WriteLine("float.MinValue: " + float.MinValue);
+            Console.WriteLine("float.NaN: " + float.NaN);
+            Console.WriteLine("float.NegativeInfinity: " + float.NegativeInfinity);
+            Console.WriteLine("float.PositiveInfinity: " + float.PositiveInfinity);
+            Console.WriteLine();
+            Console.WriteLine("double.Epsilon: " + double.Epsilon);
+            Console.WriteLine("double.MaxValue: " + double.MaxValue);
+            Console.WriteLine("double.MinValue: " + double.MinValue);
+            Console.WriteLine("double.NaN: " + double.NaN);
+            Console.WriteLine("double.NegativeInfinity: " + double.NegativeInfinity);
+            Console.WriteLine("double.PositiveInfinity: " + double.PositiveInfinity);
+            Console.WriteLine();
+            Console.WriteLine("decimal.MaxValue: " + decimal.MaxValue);
+            Console.WriteLine("decimal.MinValue: " + decimal.MinValue);
+            Console.WriteLine("decimal.One: " + decimal.One);
+            Console.WriteLine("decimal.MinusOne: " + decimal.MinusOne);
+            Console.WriteLine("decimal.Zero: " + decimal.Zero);
+            /*
+            float.Epsilon: 1,401298E-45
+            float.MaxValue: 3,402823E+38
+            float.MinValue: -3,402823E+38
+            float.NaN: не число
+            float.NegativeInfinity: -?
+            float.PositiveInfinity: ?
+
+            double.Epsilon: 4,94065645841247E-324
+            double.MaxValue: 1,79769313486232E+308
+            double.MinValue: -1,79769313486232E+308
+            double.NaN: не число
+            double.NegativeInfinity: -?
+            double.PositiveInfinity: ?
+
+            decimal.MaxValue: 79228162514264337593543950335
+            decimal.MinValue: -79228162514264337593543950335
+            decimal.One: 1
+            decimal.MinusOne: -1
+            decimal.Zero: 0
+            */
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Types_RealsFields/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Types_RealsFields")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Types_RealsFields")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4079fc7d-2ab4-4420-b8c9-927a940f98b5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
