3c4f2ece3d9f0bd0c074b3da71c7485bb0b09573
Diman <Diman@DESKTOP-CPKMC9C>
2021-04-26 19:09:28 +0300

    0.19.0.0 + Types_RealsEpsilon

---
+++ Types_RealsEpsilon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Types_RealsEpsilon
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(double.Epsilon == double.Epsilon + double.Epsilon); // false
+            Console.WriteLine(1 == 1 + double.Epsilon); // true
+            Console.WriteLine(1 == 1 - double.Epsilon); // true
+            Console.WriteLine(0 == 0 + double.Epsilon); // false
+            Console.WriteLine(0 == 0 - double.Epsilon); // false
+            Console.WriteLine(1 - 1 == double.Epsilon); // false
+            Console.ReadKey();
+        }
+    }
+}
