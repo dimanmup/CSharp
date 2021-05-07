2457932be15d7feba0dc40bac9787f8ca9455742
Diman <Diman@DESKTOP-CPKMC9C>
2021-05-07 14:23:07 +0300

    0.23.0.0 + Reflection_XT2AttributeOfT1Instance + Reflection_XT2AttributeOfT1InstanceExe

---
+++ Reflection_XT2AttributeOfT1Instance/XMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Reflection_XT2AttributeOfT1Instance
+{
+    public static class XMethods
+    {
+        public static T GetAttribute<T>(this object t)
+        {
+            object[] f = t.GetType().GetCustomAttributes(typeof(T), false);
+
+            if (f != null && f.Length == 1)
+            {
+                return (T)f[0];
+            }
+
+            throw new Exception("Атрибут не найден.");
+        }
+    }
+}
---
+++ Reflection_XT2AttributeOfT1InstanceExe/Program.cs
@@ -0,0 +1,24 @@
+﻿using Reflection_XT2AttributeOfT1Instance;
+using System;
+using System.ComponentModel;
+
+namespace Reflection_XT2AttributeOfT1InstanceExe
+{
+    [Description("Class A.")]
+    public class A 
+    { 
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            A a = new A();
+
+            DescriptionAttribute aDesc = a.GetAttribute<DescriptionAttribute>();
+
+            Console.WriteLine(aDesc.Description); // Class A.
+            Console.ReadKey();
+        }
+    }
+}
