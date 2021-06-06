66fe661f0c221fecfa640849eb856ff516bfe256
Diman <Diman@DESKTOP-CPKMC9C>
2021-06-06 20:56:58 +0300

    0.24.0.0 Kafka_1_Producing

---
+++ Kafka_1_Producing/Program.cs
@@ -0,0 +1,43 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace Kafka_1_Producing
+{
+    class Program
+    {
+        static void Main()
+        {
+            string server = "192.168.182.136:9092";
+            
+            ProducerConfig config = new ProducerConfig 
+            {
+                BootstrapServers = server
+            };
+
+            var producer = new ProducerBuilder<Null, string>(config).Build();
+            var topicMessage = new Message<Null, string>()
+            {
+                Value = "Hello World!" 
+            };
+
+            Console.WriteLine("Enter - Produce");
+            Console.WriteLine("Escape - Exit");
+            Console.Write("Produced: ");
+
+            int produced = 0;
+            ConsoleKey consoleKey;
+            do
+            {
+                consoleKey = Console.ReadKey().Key;
+                if (consoleKey == ConsoleKey.Enter)
+                {
+                    producer.Produce("mytopic1", topicMessage);
+
+                    Console.SetCursorPosition(10, 2);
+                    Console.Write((++produced).ToString().PadRight(100, ' '));
+                }
+            }
+            while (consoleKey != ConsoleKey.Escape);
+        }
+    }
+}
