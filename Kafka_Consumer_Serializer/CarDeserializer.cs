48ced54c2a1c213cf0e62b5e0ac4ae2f2b71e11f
Diman <Diman@DESKTOP-CPKMC9C>
2021-06-22 20:32:25 +0300

    0.29.0.0 Kafka_Consumer_Serializer

---
+++ Kafka_Consumer_Serializer/CarDeserializer.cs
@@ -0,0 +1,21 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace Kafka_Consumer_Serializer
+{
+    public class CarDeserializer : IDeserializer<Car>
+    {
+        public Car Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+        {
+            if (!context.Headers.Any(x => x.Key == "user"))
+            {
+                return null;
+            }
+
+            string json = string.Join("", data.ToArray().Select(x => (char)x));
+            return JsonConvert.DeserializeObject<Car>(json);
+        }
+    }
+}
---
+++ Kafka_Consumer_Serializer/Program.cs
@@ -0,0 +1,51 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+
+namespace Kafka_Consumer_Serializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumerConfig config = new ConsumerConfig
+            {
+                BootstrapServers = "192.168.182.137:9092",
+                GroupId = DateTime.Now.ToString(),
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnablePartitionEof = true
+            };
+
+            var consumer = new ConsumerBuilder<Null, Car>(config)
+                .SetValueDeserializer(new CarDeserializer())
+                .Build();
+
+            consumer.Subscribe("mytopic1");
+
+            int n = 0;
+            while (true)
+            {
+                var consumeResult = consumer.Consume();
+
+                Console.Clear();
+                Console.WriteLine(++n);
+
+                if (consumeResult.IsPartitionEOF)
+                {
+                    Console.WriteLine("All messages have been consumed, new data awaiting...");
+                }
+                else if (consumeResult?.Message?.Value != null)
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(consumeResult.Message, Formatting.Indented));
+                }
+                else
+                {
+                    Console.WriteLine("Skipping different messages.");
+                }
+
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
