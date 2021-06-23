ff21ea64e5ab7fda91e7bab19e59fee2210d1723
Diman <Diman@DESKTOP-CPKMC9C>
2021-06-23 18:23:49 +0300

    0.30.0.0 Types_Boolean

---
+++ Types_Boolean/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Types_Boolean
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Boolean f = 0 > 1;
+            bool t = 0 < 1;
+
+            System.Console.WriteLine(false);
+            System.Console.WriteLine(bool.FalseString);
+            System.Console.WriteLine(f);
+
+            System.Console.WriteLine(true);
+            System.Console.WriteLine(bool.TrueString);
+            System.Console.WriteLine(t);
+
+            System.Console.ReadKey();
+
+            //False
+            //False
+            //False
+            //True
+            //True
+            //True
+        }
+    }
+}
