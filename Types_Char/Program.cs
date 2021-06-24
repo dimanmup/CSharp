30d31fa770498a398bf580ad5d3841b5524769a0
Diman <Diman@DESKTOP-CPKMC9C>
2021-06-24 14:37:16 +0300

    0.31.0.0 Types_Char

---
+++ Types_Char/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Types_Char
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write('1');
+            Console.Write('\n');
+            Console.Write('2');
+            Console.Write('\n');
+            Console.ReadKey();
+            //1
+            //2
+            //_
+
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.ReadKey();
+            //1
+            //2
+            //_
+
+            Console.Write('1');
+            Console.Write('\r');
+            Console.Write('2');
+            Console.ReadKey();
+            //2_
+
+            Console.WriteLine();
+            Console.WriteLine('a' + 1); // Автоматическое приведение к типу 1.
+            Console.WriteLine((char)('a' + 1));
+            Console.WriteLine('a' < 'b');
+            Console.ReadKey();
+            //'b'
+            //True
+        }
+    }
+}
