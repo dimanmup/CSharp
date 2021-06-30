779af7eecbdf12510b20d8e15ada2f81c6ddb2d3
Diman <Diman@DESKTOP-CPKMC9C>
2021-06-30 13:50:48 +0300

    0.33.0.0 Types_EqualityOperators

---
+++ Types_EqualityOperators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Types_EqualityOperators
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 1;
+            int j = 1;
+            object I = 1;
+            object J = 1;
+
+            // True
+            Console.WriteLine(i == j);
+            Console.WriteLine(i == 1);
+            Console.WriteLine(1 == 1);
+            Console.WriteLine(i != 2);
+            Console.WriteLine(1 != 2);
+            Console.WriteLine(I != J);
+            Console.WriteLine(null == null);
+            Console.WriteLine();
+
+            // False
+            Console.WriteLine(I == J);
+            Console.WriteLine(i != j);
+            Console.WriteLine(i != 1);
+
+            // Error
+            // Не может сравнивать ссылку и значение.
+            //Console.WriteLine(I == i);
+            //Console.WriteLine(I == 1);
+
+            Console.ReadKey();
+        }
+    }
+}
