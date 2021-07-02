d3936ee6a98e107c8061a1decfe36fb9ae62c628
Diman <Diman@DESKTOP-CPKMC9C>
2021-07-02 13:30:07 +0300

    0.35.0.0 Thread_Creation

---
+++ Thread_Creation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Creation
+{
+    /// <summary>
+    /// Дочерний поток th переживает основной поток main.
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("main start");
+
+            Thread th = new Thread(thEp);
+            th.Start();
+
+            int i = 0;
+            while (i++ < 30)
+            {
+                Console.Write('.');
+                Thread.Sleep(200);
+            }
+
+            Console.WriteLine("\nmain stop");
+        }
+
+        static void thEp()
+        {
+            Console.WriteLine("\nthEp start");
+
+            while (true)
+            {
+                Console.Write('*');
+                Thread.Sleep(500);
+            }
+        }
+
+        //main start
+        //.
+        //thEp start
+        //*..*..*...*..*...*..*...*..*...*..*...*..*
+        //main stop
+        //**************
+    }
+}
