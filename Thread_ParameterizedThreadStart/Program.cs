a89737ef4979c57fe7f3bb8051a4069b5e90ba4a
Diman <Diman@DESKTOP-CPKMC9C>
2021-07-02 13:42:49 +0300

    0.36.0.0 Thread_ParameterizedThreadStart

---
+++ Thread_ParameterizedThreadStart/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_ParameterizedThreadStart
+{
+    class Program
+    {
+        static void Main()
+        {
+            ParameterizedThreadStart thArg = new ParameterizedThreadStart(thEp);
+            Thread th = new Thread(thArg);
+            th.Start('@');
+        }
+
+        static void thEp(object @char)
+        {
+            while (true)
+            {
+                Console.Write((char)@char);
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
