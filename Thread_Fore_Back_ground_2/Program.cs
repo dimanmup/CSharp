0952c6e6b61cd694fddfbb2e358e34e40f23510c
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-13 18:04:22 +0300

    0.39.0.0 Thread_Fore_Back_ground_2

---
+++ Thread_Fore_Back_ground_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Fore_Back_ground_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Thread fg = new Thread(foregroundThread);
+           
+            Thread bg = new Thread(backgroundThread);
+            bg.IsBackground = true;
+
+            fg.Start();
+            bg.Start();            
+        }
+
+        static void foregroundThread()
+        {
+            for (int i = 5; i >= 0; i--)
+            {
+                Thread.Sleep(500);
+                Console.Write(i);
+            }
+        }
+
+        static void backgroundThread()
+        {
+            while (true)
+            {
+                Console.Write('*');
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
