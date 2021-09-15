2be430e53d4c99c81081b1c1b516b575f5637e80
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-15 16:36:05 +0300

    0.40.0.0 Thread_IdProperties, Thread_IsAlive

---
+++ Thread_IdProperties/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_IdProperties
+{
+    class Program
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.Name = "Main";
+            WriteInfo(Thread.CurrentThread);
+
+            Thread th = new Thread(MyThread);
+            th.Start();
+            Thread.Sleep(100);
+
+            WriteInfo(th);
+
+            Console.ReadKey();
+
+            //id: 1
+            //name: Main
+
+            //id: 3
+            //name: My Thread
+        }
+
+        static void WriteInfo(Thread th)
+        {
+            Console.WriteLine("id: " + th.ManagedThreadId);
+            Console.WriteLine("name: " + th.Name);
+            Console.WriteLine();
+        }
+
+        static void MyThread()
+        {
+            Thread.CurrentThread.Name = "My Thread";
+        }
+    }
+}
---
+++ Thread_IsAlive/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_IsAlive
+{
+    class Program
+    {
+        static void Main()
+        {
+            Thread th = new Thread(MyThread);
+
+            Console.WriteLine(th.IsAlive); // False
+
+            th.Start();
+            Thread.Sleep(100);
+
+            Console.WriteLine(th.IsAlive); // True
+
+            Thread.Sleep(300);
+
+            Console.WriteLine(th.IsAlive); // False
+
+            Console.ReadKey();
+        }
+        static void MyThread()
+        {
+            Thread.Sleep(200);  
+        }
+    }
+}
