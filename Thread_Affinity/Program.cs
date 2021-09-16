e3c814e19dab5415d65a5aeed7b740a0342e4bbd
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-16 23:37:00 +0300

    0.45.0.0 Thread_Affinity

---
+++ Thread_Affinity/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Thread_Affinity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(15));
+
+            // off(CPU3) | on(CPU2) | on(CPU1) | off(CPU0) = 0110 = 6(dec)
+            Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)6;
+
+            while (true);
+        }
+    }
+}
