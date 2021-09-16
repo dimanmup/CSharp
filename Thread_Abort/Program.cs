79dbe2ec3b3e0b92e2a19253419836af5bf47725
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-16 15:31:27 +0300

    0.42.0.0 Thread_Abort, Thread_AbortPar

---
+++ Thread_Abort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Abort
+{
+    class Program
+    {
+        static Thread aborted = new Thread(Aborted);
+        static Thread aborter = new Thread(Aborter);
+
+        static void Main()
+        {
+            aborted.Start();
+            aborter.Start();
+            aborted.Join();
+
+            Console.Write($"aborted (from thread {Thread.CurrentThread.ManagedThreadId}).");
+            Console.ReadKey();
+
+            //started (from thread 3).*********aborted (from thread 1).
+        }
+
+        static void Aborted()
+        {
+            Console.Write($"started (from thread {Thread.CurrentThread.ManagedThreadId}).");
+
+            while (true)
+            {
+                Thread.Sleep(200);
+                Console.Write('*');
+            }
+        }
+
+        static void Aborter()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            aborted.Abort();
+        }
+    }
+}
---
+++ Thread_AbortPar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_AbortPar
+{
+    class Program
+    {
+        static Thread aborted = new Thread(Aborted);
+        static Thread aborter = new Thread(Aborter);
+
+        static void Main()
+        {
+            aborted.Start();
+            aborter.Start();
+            aborted.Join();
+            Console.ReadKey();
+
+            //started (from thread 3).*********aborted (from thread 4).
+        }
+
+        static void Aborted()
+        {
+            try
+            {
+                Console.Write($"started (from thread {Thread.CurrentThread.ManagedThreadId}).");
+
+                while (true)
+                {
+                    Thread.Sleep(200);
+                    Console.Write('*');
+                }
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.Write(e.ExceptionState);
+            }            
+        }
+
+        static void Aborter()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            aborted.Abort($"aborted (from thread {Thread.CurrentThread.ManagedThreadId}).");
+        }
+    }
+}
