07c5a1ec5e6ef53ef39713e0a23ac4615befde03
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-16 14:56:50 +0300

    0.41.0.0 Thread_Join

---
+++ Thread_Join/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Join
+{
+    class Program
+    {
+        static Thread th1 = new Thread(Th1);
+        static Thread th2 = new Thread(Th2);
+
+        static void Main()
+        {
+            th1.Start();
+            th2.Start();
+
+            th2.Join();
+            For('.');
+
+            Console.ReadKey();
+            //*****+++++.....
+        }
+
+        static void For(char symbol)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(100);
+                Console.Write(symbol);
+            }
+        }
+
+        static void Th1()
+        {
+            For('*');
+        }
+
+        static void Th2()
+        {
+            th1.Join();
+            For('+');
+        }
+    }
+}
