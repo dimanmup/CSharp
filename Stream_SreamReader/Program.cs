a6564f82326d87c8234ae25a2c8907b8064ae54a
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-19 13:53:34 +0300

    0.49.0.0 Stream_SreamReader

---
+++ Stream_SreamReader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Stream_SreamReader
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Diman\Desktop\numbers.txt");
+
+            int b = sr.Read();
+            while (b != -1)
+            {
+                Console.Write((char)b);
+                b = sr.Read();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
