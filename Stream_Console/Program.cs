0e543210ec529a6a5b167f73a8540654908a9c07
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-19 13:19:00 +0300

    0.48.0.0 Stream_Console

---
+++ Stream_Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stream_Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = Console.In.ReadLine(); // Ввести "123".
+            Console.Out.WriteLine(s); // Вывод "123".
+            Console.ReadKey();
+        }
+    }
+}
