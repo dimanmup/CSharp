c62b682fb558e4afb498e9be049bf4214ddad61c
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-19 14:00:21 +0300

    0.50.0.0 Stream_StreamReader_2Threads

---
+++ Stream_StreamReader_2Threads/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Stream_StreamReader_2Threads
+{
+    class Program
+    {
+        static Random rnd = new Random();
+
+        static void Main()
+        {
+            Thread reading1 = new Thread(Readind);
+            Thread reading2 = new Thread(Readind);
+
+            reading1.Start();
+            reading2.Start();
+
+            reading1.Join();
+            reading2.Join();
+
+            Console.ReadKey();
+        }
+
+        static void Readind()
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Diman\Desktop\numbers.txt");
+
+            int b = sr.Read();
+            while (b != -1)
+            {
+                Thread.Sleep(rnd.Next(0, 1000));
+                Console.Write((char)b);
+                b = sr.Read();
+            }
+        }
+    }
+}
