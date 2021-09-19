921d40da9802234609f3bae9ac27c0a6d80eed33
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-20 00:04:57 +0300

    0.52.0.0 Stream_SreamReader

--- Stream_SreamReader/Program.cs
+++ Stream_SreamReader/Program.cs
@@ -7,16 +7,31 @@ namespace Stream_SreamReader
     {
         static void Main()
         {
-            StreamReader sr = new StreamReader(@"C:\Users\Diman\Desktop\numbers.txt");
-
-            int b = sr.Read();
-            while (b != -1)
+            string path = "AZ.txt";
+            using (StreamWriter sw = new StreamWriter(path))
             {
-                Console.Write((char)b);
-                b = sr.Read();
+                for (int i = 'A'; i <= 'Z'; i++)
+                {
+                    sw.Write((char)i);
+                }
             }
 
+            string s = "";
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int b = sr.Read();
+                while (b != -1)
+                {
+                    s += (char)b;
+                    Console.Write((char)b);
+                    b = sr.Read();
+                }
+            }            
+
+            Console.WriteLine(s);
             Console.ReadKey();
+            //ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
         }
     }
 }
