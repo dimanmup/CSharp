ceffabc169c1e92fbc30c076f546851b3dfd08db
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-20 00:26:01 +0300

    0.53.0.0 Stream_SreamReaderOnSreamReader

---
+++ Stream_SreamReaderOnSreamReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Stream_SreamReaderOnSreamReader
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "AZ.txt";
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 'A'; i <= 'Z'; i++)
+                {
+                    sw.Write((char)i);
+                }
+            }
+
+            using (StreamReader sr1 = new StreamReader(path))
+            using (StreamReader sr2 = new StreamReader(sr1.BaseStream))
+            {
+                while (sr2.Peek() != -1)
+                {
+                    Console.Write((char)sr2.Read());
+                }
+            }
+
+            Console.ReadKey();
+            //ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        }
+    }
+}
