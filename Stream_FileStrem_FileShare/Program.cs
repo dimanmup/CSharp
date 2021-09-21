9d150a2f9fbd954bcf298872a5c59d00991efdd3
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-22 01:32:52 +0300

    0.57.0.0 Stream_FileStrem_FileShare

--- Stream_FileStrem_FileShare/Program.cs
+++ Stream_FileStrem_FileShare/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Console_Draft
+namespace Stream_FileStrem_FileShare
 {
     class Program
     {
