058c3ea40aa1a094d8c51248597d94ff04975beb
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-21 13:49:51 +0300

    0.55.0.0 Stream_CopyTo

---
+++ Stream_CopyTo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace Stream_CopyTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = "AZ1.txt";
+            string path2 = "AZ2.txt";
+            CreateTestFile(path1);
+
+            using (StreamWriter sw = new StreamWriter(path2))
+            using (StreamReader sr = new StreamReader(path1))
+            {
+                sr.BaseStream.CopyTo(sw.BaseStream);
+            }
+        }
+
+        static void CreateTestFile(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 'A'; i <= 'Z'; i++)
+                {
+                    sw.Write((char)i);
+                }
+            }
+        }
+    }
+}
