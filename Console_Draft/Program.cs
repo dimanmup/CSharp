491fad43b12cbf140d984ac2f83d3f41ca3d8587
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-21 17:44:11 +0300

    0.56.0.0 Stream_FileStream_Seek

---
+++ Console_Draft/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Draft
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "text.txt";
+            CreateTestFile(path);
+
+            using (FileStream fs = new FileStream("text.txt", FileMode.Open, FileAccess.Read))
+            {
+                for (long offset = 1; offset <= fs.Length; offset++)
+                {
+                    fs.Seek(-offset, SeekOrigin.End);
+                    Console.Write((char)fs.ReadByte());
+                }
+            }
+
+            Console.ReadKey();
+            //ZYXWVUTSRQPONMLKJIHGFEDCBA
+        }
+
+        static void CreateTestFile(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 'A'; i <= 'Z'; i++)
+                {
+                    sw.Write((char)i);
+                }
+            }
+        }
+    }
+}
---
+++ Stream_FileStream_Seek/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Stream_FileStream_Seek
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "text.txt";
+            CreateTestFile(path);
+
+            using (FileStream fs = new FileStream("text.txt", FileMode.Open, FileAccess.Read))
+            {
+                for (long offset = 1; offset <= fs.Length; offset++)
+                {
+                    fs.Seek(-offset, SeekOrigin.End);
+                    Console.Write((char)fs.ReadByte());
+                }
+            }
+
+            Console.ReadKey();
+            //ZYXWVUTSRQPONMLKJIHGFEDCBA
+        }
+
+        static void CreateTestFile(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 'A'; i <= 'Z'; i++)
+                {
+                    sw.Write((char)i);
+                }
+            }
+        }
+    }
+}
