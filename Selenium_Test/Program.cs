396dcee305dbe45696cd7c203bc44c386a5f11e1
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-24 18:39:10 +0300

    0.63.0.0 Selenium_Test

---
+++ Selenium_Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Selenium_Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            IWebDriver chrome = new ChromeDriver(@"C:\Program Files\Google\Chrome\Application");
+            chrome.Url = "https://neomaster.net";
+            var navigation = chrome.Navigate();
+            Thread.Sleep(2000);
+            navigation.GoToUrl("https://neomaster.net/Audit");
+            Thread.Sleep(2000);
+        }
+    }
+}
