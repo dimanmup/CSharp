e7ae28820e07114c9f60fa906335752684adf2e8
Diman <Diman@DESKTOP-CPKMC9C>
2021-09-26 00:09:48 +0300

    0.64.0.0 Tor_TryConnection

--- Console_Draft/Program.cs
+++ Console_Draft/Program.cs
@@ -1,11 +1,17 @@
-﻿using Ionic.Zip;
+﻿using DotNetTor.SocksPort;
+using Ionic.Zip;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+
 
 
 namespace Console_Draft
@@ -14,6 +20,33 @@ namespace Console_Draft
     {
         static void Main()
         {
+            var requestUri = "http://icanhazip.com/";
+            using (var httpClient = new HttpClient())
+            {
+                var message = httpClient.GetAsync(requestUri).Result;
+                var content = message.Content.ReadAsStringAsync().Result;
+                Console.WriteLine($"Your real IP: \t\t{content}");
+            }
+            
+            // 2. Get Tor IP
+            using (var httpClient = new HttpClient(new SocksPortHandler("127.0.0.1", socksPort: 9050)))
+            {
+                var message = httpClient.GetAsync(requestUri).Result;
+                var content = message.Content.ReadAsStringAsync().Result;
+                Console.WriteLine($"Your Tor IP: \t\t{content}");
+
+                // 3. Change Tor IP
+                var controlPortClient = new DotNetTor.ControlPort.Client("127.0.0.1", controlPort: 9051, password: "ILoveBitcoin21");
+                controlPortClient.ChangeCircuitAsync().Wait();
+
+                // 4. Get changed Tor IP
+                message = httpClient.GetAsync(requestUri).Result;
+                content = message.Content.ReadAsStringAsync().Result;
+                Console.WriteLine($"Your other Tor IP: \t{content}");
+            }
+            Console.ReadKey();
+
+
             //string file = "x.pdf";
             //string zip = "x.zip";
 
---
+++ Tor_TryConnection/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Tor_TryConnection
+{
+    class Program
+    {
+        /// <summary>
+        /// Проверяет соединение с Tor.
+        /// 
+        /// УСТАНОВКА.
+        /// 
+        /// Во "Включение или отключение компонентов Windows" включить Telnet Client.
+        /// 
+        /// Скопировать в папку с tor.exe файл torrc-defaults.
+        /// В cmd.exe перейти в папку с tor.exe 
+        /// (cd "C:\Users\Diman\Desktop\Tor Browser\Browser\TorBrowser\Tor").
+        /// cmd.exe: tor.exe --hash-password "пароль" | more
+        /// 
+        /// Добавить в torrc-defaults 2 строки:
+        /// ControlPort 9151
+        /// HashedControlPassword 16:[хэш пароля]
+        /// 
+        /// cmd.exe: tor.exe -f .\torrc-defaults
+        /// cmd.exe(2): telnet localhost 9151
+        /// В cmd.exe(2) пустой экран.
+        /// 
+        /// После смены пароля выйти из системы.
+        /// </summary>
+        /// <returns>true, если соединение устанавливается.</returns>
+        public static bool TryConnection(string password, out string error)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9151);
+
+            using (Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    server.Connect(endPoint);
+                }
+                catch (Exception e)
+                {
+                    error = e.Message;
+                    return false;
+                }                
+
+                // Аутентификация.
+                server.Send(Encoding.ASCII.GetBytes($"AUTHENTICATE \"{password}\"" + Environment.NewLine));
+                byte[] data = new byte[1024];
+                int receivedDataLength = server.Receive(data);
+                string receivedData = Encoding.ASCII.GetString(data, 0, receivedDataLength);
+                if (!receivedData.StartsWith("250 OK"))
+                {
+                    error = "Аутентификация не пройдена.";
+                    return false;
+                }
+
+                // Запрос новой Identity.
+                server.Send(Encoding.ASCII.GetBytes("SIGNAL NEWNYM" + Environment.NewLine));
+                data = new byte[1024];
+                receivedDataLength = server.Receive(data);
+                receivedData = Encoding.ASCII.GetString(data, 0, receivedDataLength);
+
+                if (!receivedData.Contains("250 OK"))
+                {
+                    server.Shutdown(SocketShutdown.Both);
+                    error = "Не удалось получить новую личность.";
+                    return false;
+                }
+            }
+
+            error = null;
+            return true;
+        }
+
+        static void Main()
+        {
+            //cd "C:\Users\Diman\Desktop\Tor Browser\Browser\TorBrowser\Tor"
+            //tor.exe --hash-password "Dm2411" | more
+            //tor.exe -f .\torrc-defaults
+
+            string error;
+            if (!TryConnection("Dm2411", out error))
+            {
+                Console.WriteLine(error);
+            }
+            else
+            {
+                Console.WriteLine("Работает!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
