9ae5dc3fef70ecd577146fce4c59397154f833ea
Diman <Diman@DESKTOP-CPKMC9C>
2021-10-02 21:46:07 +0300

    0.75.0.0 Thread_Monitor_TickTock

---
+++ Thread_Monitor_TickTock/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Monitor_TickTock
+{
+    class TickTock
+    {
+        int tickFrequency;
+        int tockFrequency;
+        int duration;
+        int pause;
+
+        public TickTock(int tickFrequency, int tockFrequency, int duration, int pause)
+        {
+            this.tickFrequency = tickFrequency;
+            this.tockFrequency = tockFrequency;
+            this.duration = duration;
+            this.pause = pause;
+        }
+
+        public void Beep(int frequency)
+        {
+            lock (this)
+            {
+                while (true)
+                {
+                    Console.Beep(frequency, duration);
+                    Thread.Sleep(pause);
+                    Monitor.Pulse(this);
+                    Monitor.Wait(this);
+                }
+            }
+        }
+
+        public void Run()
+        {
+            Thread tick = new Thread(() => Beep(tickFrequency));
+            Thread tock = new Thread(() => Beep(tockFrequency));
+
+            tick.Start();
+
+            while (tick.ThreadState != ThreadState.WaitSleepJoin)
+            {
+                continue;
+            }
+
+            tock.Start();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            TickTock tt = new TickTock(1500, 2000, 300, 500);
+            tt.Run();
+        }
+    }
+}
