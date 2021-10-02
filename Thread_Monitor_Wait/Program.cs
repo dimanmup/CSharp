78aeac18608e7089614f47d3d085a6e7f0228ab3
Diman <Diman@DESKTOP-CPKMC9C>
2021-10-02 17:32:58 +0300

    0.70.0.0 Thread_Monitor_Wait

---
+++ Thread_Monitor_Wait/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Monitor_Wait
+{
+    class Program
+    {
+        static Thread th1 = new Thread(() => WriteSymbol('*', th2));
+        static Thread th2 = new Thread(() => WriteSymbol('+', th1));
+
+        static void Main()
+        {
+            th1.Name = "T1";
+            th2.Name = "T2";
+
+            th1.Start();
+
+            // Чтобы запустить th2 вторым.
+            Thread.Sleep(10); // (*)
+
+            th2.Start();
+
+            //T1 >> T2 state is WaitSleepJoin.
+            //T1 >> ***
+            //T1 >> T1 waited.
+            //T2 >> T1 state is WaitSleepJoin.
+            //T2 >> +++
+            //T2 >> T2 waited.
+        }
+
+        static void WriteSymbol(char c, Thread waited)
+        {
+            lock (typeof(Program))
+            {
+                // Чтобы посмотреть статус th2 после блокировки потоком th1.
+                Thread.Sleep(100); // (*)
+
+                #region Статус другого потока
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.Write($"{Thread.CurrentThread.Name} >> ");
+                Console.WriteLine($"{waited.Name} state is {waited.ThreadState}.");
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                #endregion
+
+                Console.Write($"{Thread.CurrentThread.Name} >> ");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(1000);
+
+                    if (i < 3)
+                    {
+                        Console.Write(c);                      
+                        continue;
+                    }
+
+                    Console.WriteLine($"\n{Thread.CurrentThread.Name} >> {Thread.CurrentThread.Name} waited.");
+                    Monitor.Wait(typeof(Program));
+                }
+            }
+        }
+    }
+}
---
+++ Thread_Monitor_Wait2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Monitor_Wait2
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                string threadName = "T" + i;
+
+                Thread th = new Thread(() =>
+                {
+                    lock (typeof(Program))
+                    {
+                        for (int j = 0; j < 10; j++)
+                        {
+                            Console.WriteLine($"{threadName } >> {j}");
+
+                            if (j == 5)
+                            {
+                                Console.WriteLine($"{threadName } >> waited.\n");
+                                Monitor.Wait(typeof(Program));
+                            }
+                        }
+                    }
+                });
+
+                th.Start();
+            }
+
+            //T0 >> 0
+            //T0 >> 1
+            //T0 >> 2
+            //T0 >> 3
+            //T0 >> 4
+            //T0 >> 5
+            //T0 >> waited.
+
+            //T1 >> 0
+            //T1 >> 1
+            //T1 >> 2
+            //T1 >> 3
+            //T1 >> 4
+            //T1 >> 5
+            //T1 >> waited.
+        }
+    }
+}
