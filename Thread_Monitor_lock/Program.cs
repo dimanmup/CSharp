adf209dcab014ad6ba42cb62845913deb7c35183
Diman <Diman@DESKTOP-CPKMC9C>
2021-10-02 14:19:45 +0300

    0.67.0.0 Thread_Monitor_lock

---
+++ Thread_Monitor_lock/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Monitor_lock
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static string res = "";
+        static int threadNumber = 5;
+
+        static void Main()
+        {
+            for (int i = 0; i < threadNumber; i++)
+            {
+                Thread writer = new Thread(WriteAZ);
+                writer.Start();
+            }
+
+            while (threadNumber != 0) ;
+            Console.WriteLine(res);
+            Console.ReadKey();
+            //ABCDEF ABCDEF ABCDEF ABCDEF ABCDEF
+        }
+
+        static void WriteAZ()
+        {
+            Monitor.Enter(typeof(Program)); // lock(typeof(Program)) {
+
+            for (byte i = (byte)'A'; i <= (byte)'F'; i++)
+            {
+                Thread.Sleep(rnd.Next(200));
+                res += (char)i;
+            }
+
+            Monitor.Exit(typeof(Program)); // }
+
+            res += " ";
+            threadNumber--;
+        }
+    }
+}
