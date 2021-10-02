f595cc6b7db2a76920c36119bdc718b302e4fab7
Diman <Diman@DESKTOP-CPKMC9C>
2021-10-02 20:04:19 +0300

    0.74.0.0 Thread_Monitor_IsEntered

---
+++ Thread_Monitor_IsEntered/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Monitor_IsEntered
+{
+    class Program
+    {
+        static void Main()
+        {
+            Thread th = new Thread(() => 
+            {
+                Console.WriteLine(Monitor.IsEntered(typeof(Program))); // false
+
+                lock (typeof(Program))
+                {
+                    Console.WriteLine(Monitor.IsEntered(typeof(Program))); // true
+                }
+
+                Console.WriteLine(Monitor.IsEntered(typeof(Program))); // false
+            });
+
+            th.Start();
+            th.Join();
+            Console.ReadKey();
+        }
+    }
+}
