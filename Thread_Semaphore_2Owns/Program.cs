15d0643f1a6db93424288a9eb657d991cbc7ac30
Diman <Diman@DESKTOP-CPKMC9C>
2021-10-08 17:26:13 +0300

    0.81.0.0 Thread_Semaphore_2Owns

---
+++ Thread_Semaphore_2Owns/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Semaphore_2Owns
+{
+    class Program
+    {
+        static Semaphore sem = new Semaphore(2, 2);
+
+        static void Main()
+        {
+            Thread[] ths = new Thread[5];
+
+            for (int i = 0; i < ths.Length; i++)
+            {
+                string thIndicator = i.ToString();
+
+                ths[i] = new Thread(() =>
+                {
+                    sem.WaitOne();
+
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Console.Write(thIndicator);
+                        Thread.Sleep(100);
+                    }
+
+                    sem.Release();
+                });
+
+                ths[i].Start();
+            }
+
+            for (int i = 0; i < ths.Length; i++)
+            {
+                ths[i].Join();
+            }
+
+            Console.ReadKey();
+            //01010101010101010101232323322323323232324444444444
+        }
+    }
+}
