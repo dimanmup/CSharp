7ffd020e7019355b1436c49b10f04d7210b9e020
Diman <Diman@DESKTOP-CPKMC9C>
2021-10-11 20:56:39 +0300

    0.83.0.0 Stream_FileFromBlob_Sqlite

---
+++ Stream_FileFromBlob_Sqlite/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+using System.IO;
+
+namespace Stream_FileFromBlob_Sqlite
+{
+    public static class FileSqliteAdapter
+    {
+        public static void SaveBlob(
+            this SqliteConnection connection,
+            string tableName,
+            string blobColumnName,
+            string fileNameColumnName,
+            long id,
+            string destinationFolder
+            )
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+
+            string path = null;
+            using (SqliteCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = $"SELECT {fileNameColumnName} FROM {tableName} WHERE Id = {id};";
+
+                string fileName = (string)cmd.ExecuteScalar();
+                path = Path.Combine(destinationFolder, fileName);
+            }
+
+            using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            using (SqliteBlob sb = new SqliteBlob(connection, tableName, blobColumnName, id))
+            {
+                sb.CopyTo(fs);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            using (SqliteConnection connection = new SqliteConnection(@"data source = E:\test.db"))
+            {
+                connection.SaveBlob("files", "Data", "Name", 37, @"C:\Users\Diman\Desktop");
+            }
+        }
+    }
+}
