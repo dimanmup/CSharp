6eaa416240a21627908b8a7eeffeb652232000d8
Diman <Diman@DESKTOP-CPKMC9C>
2021-10-12 15:12:33 +0300

    0.84.0.0 CppDll, CppDllUser

---
+++ CppDll/CppDll.cpp
@@ -0,0 +1,8 @@
+#include "pch.h" // use stdafx.h in Visual Studio 2017 and earlier
+#include <utility>
+#include <limits.h>
+#include "CppDll.h"
+int get_1()
+{
+    return 1;
+}
---
+++ CppDllUser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CppDllUser
+{
+    class Program
+    {
+        public class MyClass
+        {
+            [DllImport(@"E:\VS\CSharp\x64\Debug\CppDll.dll", EntryPoint = "get_1")]
+            public static extern int Get1();
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyClass.Get1());
+            Console.ReadKey();
+        }
+    }
+}
