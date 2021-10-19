ff1103688baa0d99e1d7062b742792aaee616ddb
Diman <Diman@DESKTOP-CPKMC9C>
2021-10-19 21:05:49 +0300

    0.91.0.0 Process_WaitForExit, Process_WaitForExitInUserThread

---
+++ Process_WaitForExit/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Process_WaitForExit
+{
+    class Program
+    {
+        static void Main()
+        {
+            Thread th = new Thread(() =>
+            {
+                while (true)
+                {
+                    Console.Write('*');
+                    Thread.Sleep(300);
+                }
+            });
+            th.Start();
+
+            int i = 0;
+            while (true)
+            {
+                Console.Write('.');
+                Thread.Sleep(300);
+
+                if (i++ == 10)
+                {
+                    string timerPath = @"E:\VS\CSharp\Process_Toy_Timer\bin\Debug\Process_Toy_Timer.exe";
+                    Process timer = new Process();
+                    ProcessStartInfo timerProcessInfo = new ProcessStartInfo(timerPath, "5 yellow");
+                    timer.StartInfo = timerProcessInfo;
+                    timer.Start();
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    timer.WaitForExit(10000);
+                    sw.Stop();
+                    Console.Write(sw.Elapsed.TotalSeconds);
+                }
+            }
+
+            //.**.*.*.*.*.*.*.*.*.*.******************5,1220084.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.
+        }
+    }
+}
