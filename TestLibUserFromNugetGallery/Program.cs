2c7fba6e530be5bf0a33a209480b3ab51c75b876
Diman <Diman@DESKTOP-CPKMC9C>
2021-10-20 14:41:46 +0300

    0.96.0.0 TestLibUserFromNugetGallery

---
+++ TestLibUserFromNugetGallery/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Neomaster.TestLib;
+
+namespace TestLibUserFromNugetGallery
+{
+    class Program
+    {
+        static void Main()
+        {
+            Demo.Info();
+            Console.ReadKey();
+        }
+    }
+}
