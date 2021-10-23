764d4a46a7d7aec7240e932567e2b2a878f68f1e
Neomaster <the@neomaster.net>
2021-10-23 16:09:13 +0300

    0.98.0.0 CsTestOracle

---
+++ CsTestOracle/Program.cs
@@ -0,0 +1,38 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.IO;
+
+namespace CsTestOracle
+{
+    /// <summary>
+    /// ВМ с Oracle должно быть с NAT-адаптером.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string cs in args)
+            {
+                using (OracleConnection connection = new OracleConnection(cs))
+                {
+                    Console.WriteLine("\nCS: " + connection.ConnectionString);
+                    try
+                    {
+                        connection.Open();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("A connection was successfully established.");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine(e.StackTrace);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
