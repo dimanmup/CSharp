e1daf9b647044a9d4fcd471556d5234e4dff0b0b
Neomaster <the@neomaster.net>
2021-12-20 14:32:16 +0300

    0.103.0.0 GrpcService_Test

---
+++ GrpcService_Test/Services/HelloService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcService_Test.Services
+{
+    public class HelloService : Hello.HelloBase
+    {
+        private readonly ILogger<HelloService> _logger;
+        public HelloService(ILogger<HelloService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = request.Age > 25 ? "Hello " : ("Hi " + request.Name),
+                Status = 400
+            });
+        }
+    }
+}
